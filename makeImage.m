179d8ccc14a646506155839ef0fd3806bc184e12
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-09-30 13:21:57 +0200

    Use neighboring pixels

--- makeImage.m
+++ makeImage.m
@@ -1,7 +1,8 @@
 % TODO: wrap into a function
 
 function img = makeImage()
-	exploration = 0.5;
+	exploration = 0.1;
+	iterations = 2; %floor(5 * rand()) + 1;
 	
 	greymap = linspace(0, 1, 255)';
 	colormap([greymap greymap greymap]);
@@ -9,22 +10,27 @@ function img = makeImage()
 	w = 200;
 	h = 200;
 	%img = rand(w, h, 3);
-	img = 0.5 * ones(w, h, 3);
+	img = 1 * ones(w, h, 3);
 	
-	for i = 1:w
-		for j = 1:w
-			r = rand();
-			for k = 1:3
-				if(r <= exploration)
-					x = i + round(rand());
-					y = j + round(rand());
-					color = (3 * rand() + getPixel(img, x, y, k)) / 4;
-				else
-					x = i + round(rand());
-					y = j + round(rand());
-					color = getPixel(img, x, y, k);
+	for it = 1:iterations
+		for i = 1:w
+			for j = 1:w
+				r = rand();
+				for k = 1:3
+					if(r <= exploration)
+						x = i + round(rand());
+						y = j + round(rand());
+						color = (3 * rand() + getPixel(img, x, y, k)) / 4;
+					else
+						x = i + round(rand());
+						y = j + round(rand());
+						
+						me = getPixel(img, i, j, k);
+						neighbor = getPixel(img, x, y, k);
+						color = me * neighbor; %(me + neighbor) / 2;
+					end;
+					img(i, j, k) = color;
 				end;
-				img(i, j, k) = color;
 			end;
 		end;
 	end;
