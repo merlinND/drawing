8b2ce0d35a03fe2aaf58a249ab967456db70ce62
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-10-12 14:55:54 +0200

    Graceful scaling

--- script.m
+++ script.m
@@ -1,14 +1,21 @@
 addpath('./potentials')
 addpath('./blends')
 
-% Image parameters
-w = 100;
-h = 100;
+% ----- Image parameters
+scale = 2.5;
+% Image dimensions in pixels
+w = 100 * scale;
+h = 100 * scale;
+% TODO: relate to scale?
 iterations = 5;
-noise = 0.001;
+% Less noise leads to more homogeneous images
+noise = 0.0005 / (2 * scale);
+% Larger reach allows for stroke interpenetration
 reach = 2;
-% Points used to generate the convex polygon (overall shape)
+% Points used to generate the masking polygon (overall shape)
 nPoints = 10;
+
+usePotential = 0;
 
 % TODO: arbitrary 'walk' function (top left to bottom right, from center)
 
@@ -17,24 +24,26 @@ functions = randomPainter(noise, reach);
 %functions = constantColor([0.5 0.5 1]);
 
 % ----- Potential function
-%functions{4} = @(i, j) circular(w, h, i, j);
-%functions{4} = @(i, j) ellipsoid(w, h, 1.5, 0.5, i, j);
-%functions{4} = @(i, j) waves(w, h, 1, 3, i, j);
-%functions{4} = @(i, j) waves(w, h, 1, 3, i, j).^2;
-%functions{4} = @(i, j) modulo(w, h, 20, 20, i, j);
-%functions{4} = @(i, j) 1 - hyperboloid(w, h, 0.3, 0.8, i, j);
-%functions{4} = @(i, j) hyperboloid(w, h, 0.3, 0.8, i, j);
+if(usePotential)
+	%functions{4} = @(i, j) circular(w, h, i, j);
+	%functions{4} = @(i, j) ellipsoid(w, h, 1.5, 0.5, i, j);
+	%functions{4} = @(i, j) waves(w, h, 1, 3, i, j);
+	%functions{4} = @(i, j) waves(w, h, 1, 3, i, j).^2;
+	%functions{4} = @(i, j) modulo(w, h, 20, 20, i, j);
+	%functions{4} = @(i, j) 1 - hyperboloid(w, h, 0.3, 0.8, i, j);
+	%functions{4} = @(i, j) hyperboloid(w, h, 0.3, 0.8, i, j);
 
-%points = rand(nPoints, 2);
-points = getConvexPolygon(nPoints);
-mask = inPolygon(points, w, h);
-functions{4} = @(i, j) applyMask(mask, i, j);
+	%points = rand(nPoints, 2);
+	points = getConvexPolygon(nPoints);
+	mask = inPolygon(points, w, h);
+	functions{4} = @(i, j) applyMask(mask, i, j);
 
-% ----- Potential application function
-functions{5} = @product;
-%functions{5} = @divide;
-%functions{5} = @(level, color) hsvBlend(2, level, color);
-%functions{5} = @(level, color) hsvBlend(1, level, color);
+	% ----- Potential application function
+	functions{5} = @product;
+	%functions{5} = @divide;
+	%functions{5} = @(level, color) hsvBlend(2, level, color);
+	%functions{5} = @(level, color) hsvBlend(1, level, color);
+end;
 
 % ----- Run
 img = makeImage(w, h, functions, iterations);
