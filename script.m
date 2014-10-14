8a1af16776c09a72e4ec4877ca63b88e007b0764
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-10-15 00:32:22 +0200

    Tweak parameters

--- script.m
+++ script.m
@@ -2,20 +2,20 @@ addpath('./potentials')
 addpath('./blends')
 
 % ----- Image parameters
-scale = 2.5;
+scale = 2;
 % Image dimensions in pixels
 w = 100 * scale;
 h = 100 * scale;
-% TODO: relate to scale?
-iterations = 5;
+iterations = 1;
 % Less noise leads to more homogeneous images
-noise = 0.0005 / (2 * scale);
+noise = 0.1 / (4 * scale);
 % Larger reach allows for stroke interpenetration
 reach = 2;
-% Points used to generate the masking polygon (overall shape)
-nPoints = 10;
 
 usePotential = 0;
+% Points used to generate the masking polygon (overall shape)
+nPoints = 10;
+useConvexHull = 0;
 
 % TODO: arbitrary 'walk' function (top left to bottom right, from center)
 
@@ -38,8 +38,11 @@ if(usePotential)
 	%functions{4} = @(i, j) 1 - hyperboloid(w, h, 0.3, 0.8, i, j);
 	%functions{4} = @(i, j) hyperboloid(w, h, 0.3, 0.8, i, j);
 
-	%points = rand(nPoints, 2);
-	points = getConvexPolygon(nPoints);
+	if(useConvexHull)
+		points = getConvexPolygon(nPoints);
+	else
+		points = rand(nPoints, 2);
+	end;
 	mask = inPolygon(points, w, h);
 	functions{4} = @(i, j) applyMask(mask, i, j);
 
