dbfa233ef3a1924d48c0d3b0e8e1ef4de666dbe6
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-10-19 22:45:49 +0200

    Polar walk function

---
+++ walks/polarWalk.m
@@ -0,0 +1,18 @@
+function points = polarWalk(w, h)
+
+    centerI = (h+1) / 2;
+    centerJ = (w+1) / 2;
+
+    % List all the points we will have to walk
+    [gridI, gridJ] = meshgrid(1:h, 1:w);
+    gridI = reshape(gridI, numel(gridI), 1);
+    gridJ = reshape(gridJ, numel(gridJ), 1);
+    
+    [thetas, radii] = cart2pol(gridJ - centerJ, gridI - centerI);
+    sorted = sortrows([radii, thetas], [1 2]);
+
+    [pointsJ, pointsI] = pol2cart(sorted(:, 2), sorted(:, 1));
+    points = round([pointsI + centerI, pointsJ + centerJ]);
+    % TODO: clip if necessary
+
+end
