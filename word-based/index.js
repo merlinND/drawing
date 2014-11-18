32bcfc4186b26cf37511469413a37771c7346172
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-11-18 11:32:26 +0100

    Text preprocessing

--- word-based/index.js
+++ word-based/index.js
@@ -1,2 +1,57 @@
 'use strict';
-console.log("Hello world");
+
+var fs = require('fs');
+
+/**
+ * Parameters
+ */
+/** Chars to be stripped out */
+var strips = /[“”‘\?!#\.:,;()"'']*/gi;
+/** Chars to be replaced by a single space */
+var spaced = /[\n-/]+/gi;
+/** Sequences to be stripped */
+var seqs = /’s/gi;
+
+/**
+ * Functions to be applied to each word
+ */
+var trimmer = function(w) {
+  return w.trim();
+};
+var minLength = function(length) {
+  return function(w) {
+    return (w.length >= length);
+  };
+};
+var byLength = function(a, b) {
+  return a.length > b.length;
+};
+var groupByValue = function(container) {
+  return function(w) {
+    if(!container[w]) {
+      container[w] = 1;
+    }
+    else {
+      container[w] = container[w] + 1;
+    }
+  };
+};
+
+fs.readFile('./data/article.md', function(err, text) {
+  text = ('' + text)
+    .toLowerCase()
+    .replace(strips, '')
+    .replace(seqs, '')
+    .replace(spaced, ' ')
+    .trim();
+
+  var words = text.split(' ')
+    .map(trimmer)
+    .filter(minLength(1))
+    .sort(byLength);
+
+  var occurrences = {};
+  words.map(groupByValue(occurrences));
+
+  console.log(occurrences);
+});
