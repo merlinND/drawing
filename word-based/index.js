b476c2321491bc8f7884e941e5d40a594b2abe62
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-11-22 09:45:08 +0000

    More ways of grouping words

--- word-based/index.js
+++ word-based/index.js
@@ -13,19 +13,53 @@ var spaced = /[\n-/]+/gi;
 var seqs = /’s/gi;
 
 /**
- * Functions to be applied to each word
+ * Filters
  */
-var trimmer = function(w) {
-  return w.trim();
-};
+/** Reject words shorter than `length` */
 var minLength = function(length) {
   return function(w) {
     return (w.length >= length);
   };
 };
+/** Reject words not equal to `reference` */
+var byValue = function(reference) {
+  return function(w) {
+    return w === reference;
+  };
+};
+/** Whether `list` contains `value` */
+var contains = function(list, value) {
+  for (var i = 0; i < list.length; i += 1) {
+    if(list[i] === value) {
+      return true;
+    }
+  }
+  return false;
+};
+
+/**
+ * Sorts
+ */
 var byLength = function(a, b) {
   return a.length - b.length;
 };
+
+/**
+ * Mapping functions
+ */
+var trimmer = function(w) {
+  return w.trim();
+};
+/** Place a space between each char */
+var spacer = function(s) {
+  return s.replace(/([^ ])/gi, '$1 ').trim();
+};
+/** Sort the given list */
+var sorter = function(l) {
+  return l.sort();
+};
+
+/** Associate each distinct value with its occurrence count. Put the results in the object `container` */
 var groupByValue = function(container) {
   return function(w) {
     if(!container[w]) {
@@ -36,12 +70,38 @@ var groupByValue = function(container) {
     }
   };
 };
-// Place a space between each char
-var spacer = function(s) {
-  return s.replace(/([^ ])/gi, '$1 ').trim();
+/** Associate each distinct length with a list of words having this length. Put the results in the object `container` */
+var groupByLength = function(container) {
+  return function(w) {
+    if(!container[w.length]) {
+      container[w.length] = [w];
+    }
+    else {
+      container[w.length].push(w);
+    }
+  };
+};
+/** Associate each distinct `counts` value with a list of words with this number of occurrences. Put the results in the object `container` */
+var groupByCount = function(counts, container) {
+  return function(w) {
+    if(!container[counts[w]]) {
+      container[counts[w]] = [w];
+    }
+    else if(!contains(container[counts[w]], w)){
+      container[counts[w]].push(w);
+    }
+  };
 };
 
-var getWords = function(text) {
+/**
+ * Transforms
+ */
+/** Turn text into a clean list of words */
+var getWords = function(text, minimumLength) {
+  if(!minimumLength) {
+    minimumLength = 1;
+  }
+
   text = ('' + text)
     .toLowerCase()
     .replace(strips, '')
@@ -52,20 +112,29 @@ var getWords = function(text) {
   return text
     .split(' ')
     .map(trimmer)
-    .filter(minLength(1));
+    .filter(minLength(minimumLength));
 };
-
-var getUniqueByLength = function(words) {
-  // Take unique words
+/** Gives a "grid of chars" effect */
+var getSpacedWords = function(words) {
+  words = Object.keys(getWordsCount(words));
   return words
     .sort(byLength)
     .map(spacer)
     .join(' ');
 };
-
-var getWordsByLength = function(words) {
+/** For each word, get the number of occurrences */
+var getWordsCount = function(words) {
   var occurrences = {};
   words.map(groupByValue(occurrences));
+  return occurrences;
+};
+/** For each count, get the ordered list of words with that count */
+var getWordsByCount = function(words) {
+  var occurrences = {};
+  words.map(groupByCount(getWordsCount(words), occurrences));
+  Object.keys(occurrences).forEach(function(k) {
+    occurrences[k] = occurrences[k].sort();
+  });
   return occurrences;
 };
 
@@ -73,6 +142,7 @@ fs.readFile('./data/article.md', function(err, text) {
   var words = getWords(text);
 
   // console.log(words);
-  // console.log(getUniqueByLength(words));
-  console.log(getWordsByLength(words));
+  console.log(getSpacedWords(words));
+  //console.log(getWordsCount(words));
+  // console.log(getWordsByCount(words));
 });
